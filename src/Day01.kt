e133eeafc92df66014e4be541093af39ae58d889
zt64 <nwowens32@gmail.com>
2022-12-01 19:22:49 -0500

    Solve part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,13 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
+
     println(part1(input))
-    println(part2(input))
 }
+
+fun part1(input: String) = input
+    .split("\n\n")
+    .map { it.lines().filter(String::isNotBlank) }
+    .map { it.map(String::toInt) }
+    .map { it.sum() }
+    .sorted()
+    .max()
