c5dc4539d61b7f636da059534a5b64cd527af890
zt64 <nwowens32@gmail.com>
2022-12-10 12:18:30 -0500

    Solve day 10

--- src/Day.kt
+++ src/Day.kt
@@ -1,8 +1,8 @@
 abstract class Day(number: Int) {
-    val input = readInput("Day${number.toString().padStart(2, '0')}")
+    protected val input = readInput("Day${number.toString().padStart(2, '0')}")
 
-    abstract fun part1(): Any
-    abstract fun part2(): Any
+    protected abstract fun part1(): Any
+    protected abstract fun part2(): Any
 
     open fun main() {
         println("Part 1: ${part1()}")
