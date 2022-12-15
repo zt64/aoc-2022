9be4aebb21a1e4cb9f17b3bbfc818b7fc1ac1218
zt64 <nwowens32@gmail.com>
2022-12-14 21:50:34 -0500

    Solve day 13

--- src/Day13.kt
+++ src/Day13.kt
@@ -3,11 +3,17 @@ import kotlin.math.min
 private object Day13 : Day(13) {
     private fun buildPacket(string: String) = buildList<Any> {
         val stack = mutableListOf<MutableList<Any>>()
+        var tempNum = ""
 
         string.forEach { char ->
             if (char.isDigit()) {
-                stack.last() += char.toString().toInt()
+                tempNum += char.digitToInt()
             } else {
+                if (tempNum.isNotBlank()) {
+                    stack.last() += tempNum.toInt()
+                    tempNum = ""
+                }
+
                 when (char) {
                     '[' -> {
                         val last = stack.lastOrNull()
@@ -40,7 +46,24 @@ private object Day13 : Day(13) {
         return sum
     }
 
-    override fun part2(): Int = TODO("Not yet implemented")
+    override fun part2(): Int {
+        val dividerPacket1 = buildPacket("[[2]]")
+        val dividerPacket2 = buildPacket("[[6]]")
+
+        val sorted = buildList {
+            pairs.forEach { (packet1, packet2) ->
+                add(packet1)
+                add(packet2)
+            }
+
+            add(dividerPacket1)
+            add(dividerPacket2)
+
+            sortWith(List<Any>::compareTo)
+        }
+
+        return (sorted.indexOf(dividerPacket1) + 1) * (sorted.indexOf(dividerPacket2) + 1)
+    }
 }
 
 private operator fun Any.compareTo(right: Any): Int {
@@ -62,7 +85,7 @@ private operator fun Any.compareTo(right: Any): Int {
 
         left is Int && right is Int -> left.compareTo(right)
         left is Int && right is List<*> -> listOf(left).compareTo(right as List<Any>)
-        left is List<*> && right is Int -> (left  as List<Any>).compareTo(listOf(right))
+        left is List<*> && right is Int -> (left as List<Any>).compareTo(listOf(right))
         else -> throw IllegalArgumentException("Invalid input")
     }
 }
